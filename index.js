21cf8ddbcc5d119ae270ed15264457eb56404a8f
Jonathan Chen <jontkc98@uw.edu>
2021-12-28 11:18:14 -0800

    Added inquirer async function

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-let questions = [
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
     {
         type: 'input',
         name: 'description',
         message: 'What is a short description of your project?'
     },
-    {
-        type: 'input',
-        name: 'installation instruction',
-        message: 'How do you install your program?'
-    }
-    // ,
+    // {
+    //     type: 'input',
+    //     name: 'installationinstruction',
+    //     message: 'How do you install your program?'
+    // },
     // {
     //     type: 'input',
     //     name: 'usage information',
@@ -27,18 +32,27 @@ let questions = [
     //     type: 'input',
     //     name: "test instructions",
     //     message: 'Test instructions'
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like this projec to have?',
+        choices: ['GNU_AGPLv3', 'GNU_GPLv3', 'GNU_LGPLv3', 'Mozilla_Public_License_2', 'Apache_License_2', 'MIT_License', 'Boost_Software_License_1', 'The_Unlicense']
+    }
     // }
 ];
 
+let answers;
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log("Hi everything is working so far");
     inquirer
-        .prompt(questions).then((answers) => {
-            console.log(JSON.stringify(answers, null, "   "))
+        .prompt(questions).then((response) => {
+             answers = response;
+             writeToFile(`${response.title}.txt`, response);
         });
 }
 
