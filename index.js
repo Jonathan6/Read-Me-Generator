104b7cb72223e575ba5f8bee975d45722d2004a2
Jonathan Chen <jontkc98@uw.edu>
2021-12-27 20:25:35 -0800

    Created inital questions and init function

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [
-    "description",
-    "installation instruction",
-    "usage information",
-    "contribution guidlines",
-    "test instructions"
+let questions = [
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is a short description of your project?'
+    },
+    {
+        type: 'input',
+        name: 'installation instruction',
+        message: 'How do you install your program?'
+    }
+    // ,
+    // {
+    //     type: 'input',
+    //     name: 'usage information',
+    //     message: 'How do you use this program?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: "contribution guidlines",
+    //     message: 'What are the contribution guildlines for this project?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: "test instructions",
+    //     message: 'Test instructions'
+    // }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("Hi everything is working so far");
+    inquirer
+        .prompt(questions).then((answers) => {
+            console.log(JSON.stringify(answers, null, "   "))
+        });
+}
 
 // Function call to initialize app
 init();
